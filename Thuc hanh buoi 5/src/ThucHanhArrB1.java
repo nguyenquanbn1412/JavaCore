d7ee69483419c0c017cd1410caf7d6a1941df90f
nguyenquanbn1412 <148620904+nguyenquanbn1412@users.noreply.github.com>
2023-12-09 10:01:21 +0700

    BT Buoi 4

--- Thuc hanh buoi 5/src/ThucHanhArrB1.java
+++ Thuc hanh buoi 5/src/ThucHanhArrB1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ThucHanhArrayB1 {
+public class ThucHanhArrB1 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("Ban dang khoi tao mang, nhap so phan tu cua mang:");
